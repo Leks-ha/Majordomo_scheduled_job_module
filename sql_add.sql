9efd943b836a42661e42dcc1f45dcee2035ff4c3
Leks <leks@email.no>
2014-02-07 14:57:27 +0400

    change tables to InnoDb

--- sql_add.sql
+++ sql_add.sql
@@ -20,14 +20,9 @@ CREATE TABLE `scheduled_job_action` (
     FOREIGN KEY scheduled_job_id_fk (scheduled_job_id)
     REFERENCES scheduled_job (scheduled_job_id)
     ON UPDATE CASCADE ON DELETE RESTRICT
-) ENGINE=InnoDb AUTO_INCREMENT=20 DEFAULT CHARSET=utf                  
+) ENGINE=InnoDb AUTO_INCREMENT=20 DEFAULT CHARSET=utf8                  
 
                       
                       
 insert into `project_modules` (`NAME`, `TITLE`, `CATEGORY`, `PARENT_NAME`, `DATA`, `HIDDEN`, `PRIORITY`, `ADDED`) values('scheduled_job','scheduled_job','<#LANG_SECTION_SYSTEM#>','','','0','0','2014-01-20 13:02:48');
  
- 
-      
-
-               
-               8  
